bd64c0a4806408abc32b077d74d88b74e32c43b7
Sambulo Senda <sambulosendas@gmail.com>
2019-04-14 19:41:15 +0100

    sorted

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import './TourList.scss';
 import Tour from '../Tour/Tour';
+import { tourData } from '../tourData';
 
 class TourList extends Component {
-
-    state ={
-        tours:[]
+    state = {
+        tours: tourData
+    };
+    removeTour = id => {
+        const { tours } = this.state;
+        const sortedTours = tours.filter(tour => tour.id !==id);
+        this.setState({
+           tours: sortedTours
+        })
     }
+
     render() {
+        const { tours } = this.state;
+        console.log(this.state.tours);
         return (
             <div>
-                <section className = "tourList">
-                    <Tour />
+                <section className="tourList">
+                    {tours.map(tour => {
+                        return (
+                            <Tour key={tour.id} tour={tour} removeTours={this.removeTour} />
+                        )
+                    })}
                 </section>
             </div>
         );
